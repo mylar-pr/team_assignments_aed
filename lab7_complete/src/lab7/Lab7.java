ff69deb96bc7046b586f22c41e9df77597862272
devpramod <pai.pra@husky.neu.edu>
2020-02-22 19:46:45 -0500

    added another print line

--- lab7_complete/src/lab7/Lab7.java
+++ lab7_complete/src/lab7/Lab7.java
@@ -37,6 +37,7 @@ public class Lab7 {
         Lab7 inst = new Lab7();
         inst.readData();
         System.out.println("Pramod");
+        System.out.println("Another line");
     }
     
     private void readData() throws IOException{
